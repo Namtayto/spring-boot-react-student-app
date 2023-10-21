eb5e0b398fd30897a10e76fe38a92bb37103ed7d
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2023-10-21 12:44:11 +0700

    Edit apiconfig

--- Frontend/DAA/src/config/apiconfig.js
+++ Frontend/DAA/src/config/apiconfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454";
+export const API_BASE_URL = "https://studentapp-5egu.onrender.com";
 
 const jwt = localStorage.getItem("jwt");
 
