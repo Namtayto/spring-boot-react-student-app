dae0a83b245ccac2cf7774913b184c0b6386d9fc
Namtayto <namnh.uit@gmail.com>
2023-11-11 12:49:49 +0700

    Remove StudentPointController and add StudentController

--- Backend/DAA/DAA/src/main/java/com/nam/controller/StudentController.java
+++ Backend/DAA/DAA/src/main/java/com/nam/controller/StudentController.java
@@ -1,29 +1,38 @@
 package com.nam.controller;
 
+import com.nam.model.Student;
 import com.nam.model.StudentPoint;
+import com.nam.repository.StudentRepository;
 import com.nam.service.StudentPointService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/studentpoint")
-public class StudentPointController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    private final StudentRepository studentRepository;
     private final StudentPointService studentPointService;
 
-
-    public StudentPointController(StudentPointService studentPointService) {
+    @Autowired
+    public StudentController(StudentRepository studentRepository, StudentPointService studentPointService) {
+        this.studentRepository = studentRepository;
         this.studentPointService = studentPointService;
     }
-
 
     @PostMapping("/subject/{studentId}/{semester}")
     public ResponseEntity<StudentPoint> addSubject(@RequestBody StudentPoint studentPoint, @PathVariable String studentId, @PathVariable String semester) {
         StudentPoint saveStudentPoint = studentPointService.createStudentPoint(studentPoint, studentId, semester);
         return new ResponseEntity<>(saveStudentPoint, HttpStatus.CREATED);
     }
+
+    @GetMapping("/getStudentList")
+    public ResponseEntity<List<Student>> getAllNotice() {
+
+        List<Student> students = studentRepository.findAll();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
 }
-
-
-
