0e7139823119618eccf0ba5cd772e26c375c7b91
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-22 15:11:58 +0700

    Create UserDetailsImpl and rename CustomerUserService

--- Backend/DAA/DAA/src/main/java/com/nam/service/UserDetailsServiceImpl.java
+++ Backend/DAA/DAA/src/main/java/com/nam/service/UserDetailsServiceImpl.java
@@ -3,22 +3,18 @@ package com.nam.service;
 import com.nam.model.User;
 import com.nam.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
-public class CustomerUserServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private UserRepository userRepository;
 
     @Autowired
-    public CustomerUserServiceImpl(UserRepository userRepository) {
+    public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
@@ -27,9 +23,10 @@ public class CustomerUserServiceImpl implements UserDetailsService {
         User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
 
-        List<GrantedAuthority> authorities = new ArrayList<>();
-
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+//        List<GrantedAuthority> authorities = new ArrayList<>();
+//
+//        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+        return UserDetailsImpl.build(user);
     }
 }
 
