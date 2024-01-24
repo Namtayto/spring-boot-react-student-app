a22ec6377ed043518984a8f6c3bfe4f957b70945
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-24 17:18:27 +0700

    Use CustomUserDetail and create test endpoint
    Testing JWT Auth

--- Backend/DAA/DAA/src/main/java/com/nam/config/AppConfig.java
+++ Backend/DAA/DAA/src/main/java/com/nam/config/AppConfig.java
@@ -1,127 +1,127 @@
 package com.nam.config;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.nam.security.AuthEntryPointJwt;
+import com.nam.security.JwtValidator;
+import com.nam.security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
+@EnableMethodSecurity
 @EnableWebSecurity
 public class AppConfig {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public JwtValidator authenticationJwtTokenFilter() {
+        return new JwtValidator();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://studywithnam.vercel.app"));
+        cfg.setAllowedMethods(Collections.singletonList("*"));
+        cfg.setAllowCredentials(true);
+        cfg.setAllowedHeaders(Collections.singletonList("*"));
+        cfg.setExposedHeaders(Arrays.asList("Authorization"));
+        cfg.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cfg);
+        return source;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsServiceImpl);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
     @Bean
     @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .securityMatcher(AntPathRequestMatcher.antMatcher("/api/**"))
+//                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-                .authorizeHttpRequests(auth -> auth
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/test/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
-                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration cfg = new CorsConfiguration();
-                        cfg.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://studywithnam.vercel.app"));
-                        cfg.setAllowedMethods(Collections.singletonList("*"));
-                        cfg.setAllowCredentials(true);
-                        cfg.setAllowedHeaders(Collections.singletonList("*"));
-                        cfg.setExposedHeaders(Arrays.asList("Authorization"));
-                        cfg.setMaxAge(3600L);
-                        return cfg;
-
-                    }
-                }));
+                .authenticationProvider(authProvider())
+                .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
     }
 
-    @Bean
-    @Order(2)
-    public SecurityFilterChain basicAuh(HttpSecurity http) throws Exception {
-        http
-                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.disable())
-                .csrf(AbstractHttpConfigurer::disable)
-                .securityMatcher("/auth/signup/student", "/notice/create", "/tuition/**")
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().hasRole("ADMIN")
-                )
-                .httpBasic(Customizer.withDefaults());
+    //    @Bean
+    //    @Order(2)
+    //    public SecurityFilterChain basicAuh(HttpSecurity http) throws Exception {
+    //        http
+    //                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.disable())
+    //                .csrf(AbstractHttpConfigurer::disable)
+    //                .securityMatcher("/auth/signup/student", "/notice/create", "/tuition/**")
+    //                .authorizeHttpRequests(auth -> auth
+    //                        .anyRequest().hasRole("ADMIN")
+    //                )
+    //                .authenticationProvider(authenticationProvider(us()))
+    //                .httpBasic(Customizer.withDefaults());
+    //
+    //
+    //        return http.build();
+    //    }
 
-
-        return http.build();
-    }
-
-    @Bean
-    @Order(3)
-    public SecurityFilterChain other(HttpSecurity http) throws Exception {
-        http
-
-                .csrf(AbstractHttpConfigurer::disable)
-                .securityMatcher("/auth/signin", "notice/**", "teacher/**", "student/**")
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration cfg = new CorsConfiguration();
-                        cfg.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://studywithnam.vercel.app"));
-                        cfg.setAllowedMethods(Collections.singletonList("*"));
-                        cfg.setAllowCredentials(true);
-                        cfg.setAllowedHeaders(Collections.singletonList("*"));
-                        cfg.setExposedHeaders(Arrays.asList("Authorization"));
-                        cfg.setMaxAge(3600L);
-                        return cfg;
-
-                    }
-                }));
-
-
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("nam123"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(admin);
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
-        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(userDetailsService);
-        auth.setPasswordEncoder(passwordEncoder());
-        return auth;
-    }
+//    @Bean
+//    @Order(2)
+//    public SecurityFilterChain other(HttpSecurity http) throws Exception {
+//        http
+//
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .securityMatcher("/auth/signin", "notice/**", "teacher/**", "student/**", "/auth/signup/*")
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll()
+//                );
+//
+//
+//        return http.build();
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
