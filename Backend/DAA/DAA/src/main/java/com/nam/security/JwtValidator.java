77cf8652cbeafd72f7eec5314dce6ee21224314c
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-24 15:16:10 +0700

    Remove target folder

--- Backend/DAA/DAA/src/main/java/com/nam/security/JwtValidator.java
+++ Backend/DAA/DAA/src/main/java/com/nam/security/JwtValidator.java
@@ -1,5 +1,6 @@
 package com.nam.security;
 
+import com.nam.security.services.UserDetailsServiceImpl;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -7,19 +8,23 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
-import java.util.List;
 
 public class JwtValidator extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailService;
+
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String jwt = request.getHeader(JwtConstant.JWT_HEADER);
@@ -32,12 +37,16 @@ public class JwtValidator extends OncePerRequestFilter {
 
                 String email = String.valueOf(claims.get("email"));
 
-                String authorities = String.valueOf(claims.get("authorities"));
-
-                List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
-
-                Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, auths);
-
+//                String authorities = String.valueOf(claims.get("authorities"));
+//                List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+//                Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, auths);
+                UserDetails userDetails = userDetailService.loadUserByUsername(email);
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             } catch (Exception e) {
                 throw new BadCredentialsException("invalid token .... from jwt validator");
