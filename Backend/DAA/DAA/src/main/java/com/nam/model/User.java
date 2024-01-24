e18767f6763e8c461676c0a30c51bc53e3fd73ea
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-24 15:43:23 +0700

    Change role fetch type to Eager in User Model

--- Backend/DAA/DAA/src/main/java/com/nam/model/User.java
+++ Backend/DAA/DAA/src/main/java/com/nam/model/User.java
@@ -33,7 +33,7 @@ public abstract class User {
     @Email
     private String email;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
