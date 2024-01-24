986f88c5b2d7891adef816f9c0df450d5ce2f6d9
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-24 15:20:51 +0700

    Create Test Controller for testing Authorization

---
+++ Backend/DAA/DAA/src/main/java/com/nam/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.nam.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public Content.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
+
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('STUDENT')")
+    public String moderatorAccess() {
+        return "STUDENT.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('TEACHER')")
+    public String adminAccess() {
+        return "TEACHER.";
+    }
+}
