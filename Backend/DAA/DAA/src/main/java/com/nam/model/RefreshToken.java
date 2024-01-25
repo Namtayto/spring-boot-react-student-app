d7d254479359ae98c3c5752d1dc7db768539d949
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-25 14:10:00 +0700

    Create RefreshToken Model

---
+++ Backend/DAA/DAA/src/main/java/com/nam/model/RefreshToken.java
@@ -0,0 +1,31 @@
+package com.nam.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity(name = "refreshtoken")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+}
