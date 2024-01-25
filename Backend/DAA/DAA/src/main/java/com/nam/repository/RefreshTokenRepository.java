8d4d502ab1ac27aa5dd3d51548142e661570ae25
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-25 14:57:30 +0700

    Create RefreshToken Repository

---
+++ Backend/DAA/DAA/src/main/java/com/nam/repository/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.nam.repository;
+
+import com.nam.model.RefreshToken;
+import com.nam.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    int deleteByUser(User user);
+}
