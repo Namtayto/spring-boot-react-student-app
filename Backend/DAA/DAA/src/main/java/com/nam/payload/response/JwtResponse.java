cdfa6cdf0b2a2ecec29a3d300bb10610d8bf81e0
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-25 15:05:10 +0700

    Add refreshtoken field in JwtResponse and Add Lombok

--- Backend/DAA/DAA/src/main/java/com/nam/payload/response/JwtResponse.java
+++ Backend/DAA/DAA/src/main/java/com/nam/payload/response/JwtResponse.java
@@ -1,7 +1,14 @@
 package com.nam.payload.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class JwtResponse {
     private String token;
     private String type = "Bearer";
@@ -16,49 +23,5 @@ public class JwtResponse {
         this.username = username;
         this.email = email;
         this.roles = roles;
-    }
-
-    public String getAccessToken() {
-        return token;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.token = accessToken;
-    }
-
-    public String getTokenType() {
-        return type;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.type = tokenType;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public List<String> getRoles() {
-        return roles;
     }
 }
