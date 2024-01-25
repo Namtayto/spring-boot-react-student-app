cbf6590952fab9768a6fb4d4e02b3bde1fb029df
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-25 15:10:50 +0700

    Forgot Add RefreshToken Field in JwtResponse

--- Backend/DAA/DAA/src/main/java/com/nam/payload/response/JwtResponse.java
+++ Backend/DAA/DAA/src/main/java/com/nam/payload/response/JwtResponse.java
@@ -12,13 +12,15 @@ import java.util.List;
 public class JwtResponse {
     private String token;
     private String type = "Bearer";
+    private String refreshToken;
     private Long id;
     private String username;
     private String email;
     private List<String> roles;
 
-    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
+    public JwtResponse(String accessToken, String refreshToken, Long id, String username, String email, List<String> roles) {
         this.token = accessToken;
+        this.refreshToken = refreshToken;
         this.id = id;
         this.username = username;
         this.email = email;
