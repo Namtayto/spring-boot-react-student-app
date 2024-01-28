360cd3b05c43e34b7be9eab29ab504389757e29d
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-28 16:03:19 +0700

    Update request matcher to test refresh token api

--- Backend/DAA/DAA/src/main/java/com/nam/config/AppConfig.java
+++ Backend/DAA/DAA/src/main/java/com/nam/config/AppConfig.java
@@ -82,7 +82,7 @@ public class AppConfig {
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/test/**").permitAll()
+                        .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
