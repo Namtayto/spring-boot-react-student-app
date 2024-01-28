ef5a86a8a2856b8717a27e813a1e4915c3e496a4
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-01-28 15:43:29 +0700

    Create Request and Response class for Refresh Token

---
+++ Backend/DAA/DAA/src/main/java/com/nam/payload/request/TokenRefreshRequest.java
@@ -0,0 +1,13 @@
+package com.nam.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TokenRefreshRequest {
+    @NotBlank
+    private String refreshToken;
+
+}
