2b489d5e094f613df22bfd560a9456eb23503932
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-02-20 16:52:38 +0700

    Add findAllWithPagination method to StudentRepository

--- Backend/DAA/DAA/src/main/java/com/nam/repository/StudentRepository.java
+++ Backend/DAA/DAA/src/main/java/com/nam/repository/StudentRepository.java
@@ -1,9 +1,15 @@
 package com.nam.repository;
 
 import com.nam.model.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     public Student findByStudentId(String studentId);
+
+    @Query("SELECT s FROM Student s")
+    public Page<Student> findAllWithPagination(Pageable pageable);
 
 }
