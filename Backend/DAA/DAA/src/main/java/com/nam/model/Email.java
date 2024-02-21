08bc7a8e7a1df4409af3e90d7d45aaebc5343a91
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-02-21 15:20:40 +0700

    Add @Builder to Email model

--- Backend/DAA/DAA/src/main/java/com/nam/model/Email.java
+++ Backend/DAA/DAA/src/main/java/com/nam/model/Email.java
@@ -4,16 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Email {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,5 +19,5 @@ public class Email {
 
     private String registerEmail;
 
- 
+
 }
