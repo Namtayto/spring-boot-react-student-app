0e308a8ea50e0fd9d6d9fb9e40890e510e481f01
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-02-26 09:53:40 +0700

    Add method getStudentPointBySemesterAndStudentId for StudentPointRepository

--- Backend/DAA/DAA/src/main/java/com/nam/repository/StudentPointRepository.java
+++ Backend/DAA/DAA/src/main/java/com/nam/repository/StudentPointRepository.java
@@ -2,9 +2,15 @@ package com.nam.repository;
 
 import com.nam.model.StudentPoint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StudentPointRepository extends JpaRepository<StudentPoint, Long> {
     public StudentPoint findBySemester(String semester);
 
+    @Query("SELECT sp FROM StudentPoint sp" +
+            " JOIN sp.student s" +
+            " WHERE s.studentId = :studentId AND sp.semester = :semester")
+    public StudentPoint getStudentPointBySemesterAndStudentId(@Param("studentId") String studentId, @Param("semester") String semester);
 
 }
