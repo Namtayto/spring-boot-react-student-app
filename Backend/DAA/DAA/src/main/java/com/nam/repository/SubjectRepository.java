03335199da75d3da1ac1c5e2fe1c58fcc5919261
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-02-27 14:36:00 +0700

    Remove findById method in SubjectRepository
    Don't need to use

--- Backend/DAA/DAA/src/main/java/com/nam/repository/SubjectRepository.java
+++ Backend/DAA/DAA/src/main/java/com/nam/repository/SubjectRepository.java
@@ -3,8 +3,5 @@ package com.nam.repository;
 import com.nam.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
-    public Optional<Subject> findById(Long id);
 }
