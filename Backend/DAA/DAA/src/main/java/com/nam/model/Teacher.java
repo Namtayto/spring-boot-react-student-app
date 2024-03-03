d520755dfc7b0c872d7c5c70520c64c9a3247f9f
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-03-03 15:03:01 +0700

    I forgot that the teacher model doesn't have any local variables, so delete the @NoArgsConstructor.

--- Backend/DAA/DAA/src/main/java/com/nam/model/Teacher.java
+++ Backend/DAA/DAA/src/main/java/com/nam/model/Teacher.java
@@ -3,14 +3,12 @@ package com.nam.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @PrimaryKeyJoinColumn(name = "id")
-@SuperBuilder
 @AllArgsConstructor
-@NoArgsConstructor
+@SuperBuilder
 public class Teacher extends User {
 
 }
