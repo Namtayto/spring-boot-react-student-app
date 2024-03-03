eff99004d0905fdffbe68f4ece2fe3384db08c40
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-03-03 14:47:59 +0700

    Add @SuperBuilder and Constructor for teacher model

--- Backend/DAA/DAA/src/main/java/com/nam/model/Teacher.java
+++ Backend/DAA/DAA/src/main/java/com/nam/model/Teacher.java
@@ -2,9 +2,15 @@ package com.nam.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @PrimaryKeyJoinColumn(name = "id")
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Teacher extends User {
 
 }
